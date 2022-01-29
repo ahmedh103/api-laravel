f6caeb51fdc66f590d9fb0ea9605cc35d5a10367
ahmedh103 <ahmedhassanfawzy595@gmail.com>
2022-01-29 16:51:17 +0200

    secound commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+      return product::all();
+    }
+
+   
+
+   
+    public function store(Request $request)
+    {
+       $request->validate([
+
+        "name"=>"required|string",
+        "price"=>"required|numeric"
+
+       ]);
+
+       return product::create( $request->all());
+    }
+
+ 
+    public function show($id)
+    {
+        return product::find($id);
+       
+    }
+
+  
+
+  
+    public function update(Request $request, $id)
+    {
+  $product = product::find($id);
+  $product->update($request->all());   
+  return $product ;
+    }
+
+
+    public function destroy($id)
+    {
+        return Product::destroy($id) ;
+    }
+}
